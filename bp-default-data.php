93379f07fa6a348a8eb8dcba60364490e8eb4b4f
Paul Gibbs <paul@byotos.com>
2012-11-04 12:29:09 +0000

    Move admin menu underneath Settings
    The top-level BP menu is deprecated

--- bp-default-data.php
+++ bp-default-data.php
@@ -22,7 +22,7 @@ function bpdd_admin_page() {
 	if ( ! is_super_admin() )
 		return;
 
-	$page = add_submenu_page( 'bp-general-settings', __( 'Default Data', 'bpdd' ), __( 'Default Data', 'bpdd' ), 'manage_options', 'bpdd-setup', 'bpdd_admin_page_content'  );
+	$page = add_options_page( __( 'BuddyPress Default Data', 'bpdd' ), __( 'BP Default Data', 'bpdd' ), 'manage_options', 'bpdd-setup', 'bpdd_admin_page_content' );
 	add_action( "admin_print_styles-$page", 'bp_core_add_admin_menu_styles' );
 }
 
